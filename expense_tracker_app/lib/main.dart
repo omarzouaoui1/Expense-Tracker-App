0b69a9b861d6e8ae85ce19507831405ded2c2c57
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-09 18:41:27 +0100

    Rendring a text on the show modal bottom sheet

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
+    // theme: ThemeData(useMaterial3: false),
     home: Expenses(),
   ));
 }
--- expense_tracker_app/lib/widgets/expenses.dart
+++ expense_tracker_app/lib/widgets/expenses.dart
@@ -37,20 +37,27 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Modal bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           )
         ],
       ),
       body: Column(
         children: [
-          const Text('Expenses page'),
           Expanded(
             child: ExpensesList(
               expenses: _registeredExpenses,
