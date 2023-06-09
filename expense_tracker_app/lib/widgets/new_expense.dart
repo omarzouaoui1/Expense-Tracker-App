81797c8c9cc0ac8fc878327ad2f1ca4e3bb2ca8b
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-09 19:09:05 +0100

    getting user input

--- expense_tracker_app/lib/widgets/new_expense.dart
+++ expense_tracker_app/lib/widgets/new_expense.dart
@@ -8,17 +8,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _entredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _entredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_entredTitle);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
           )
         ],
       ),
