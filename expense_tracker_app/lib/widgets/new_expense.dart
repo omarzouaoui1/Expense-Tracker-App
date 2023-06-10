7e23ac0cf88a9b26c90538b1b7c424ec9d4daf36
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-10 06:46:15 +0100

    amount input + cancel button

--- expense_tracker_app/lib/widgets/new_expense.dart
+++ expense_tracker_app/lib/widgets/new_expense.dart
@@ -9,10 +9,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -29,13 +31,27 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
               ),
             ],
           )
