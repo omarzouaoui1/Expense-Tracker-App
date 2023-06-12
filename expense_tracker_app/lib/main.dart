254eeab978b7ffea80468f756e12f5bbf1b9f9d6
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-12 09:27:58 +0100

    playing with theming

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var colorShceme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 94, 136, 184),
+);
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData().copyWith(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Color.fromARGB(255, 111, 212, 203)),
+      useMaterial3: true,
+      colorScheme: colorShceme,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: colorShceme.onPrimaryContainer,
+        foregroundColor: colorShceme.primaryContainer,
+        centerTitle: true,
+      ),
+    ),
     home: const Expenses(),
   ));
 }
