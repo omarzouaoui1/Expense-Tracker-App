fa7d98f7a36208d432cd364e5284ceac6d3607c3
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-12 09:50:41 +0100

    buttons & texts theming

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var colorShceme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 94, 136, 184),
+  seedColor: Color.fromARGB(255, 230, 145, 19),
 );
 
 void main() {
@@ -11,11 +11,30 @@ void main() {
     theme: ThemeData().copyWith(
       useMaterial3: true,
       colorScheme: colorShceme,
-      appBarTheme: AppBarTheme().copyWith(
+      appBarTheme: const AppBarTheme().copyWith(
         backgroundColor: colorShceme.onPrimaryContainer,
         foregroundColor: colorShceme.primaryContainer,
-        centerTitle: true,
+        // centerTitle: true,
       ),
+      cardTheme: const CardTheme().copyWith(
+        color: colorShceme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 9,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: colorShceme.primaryContainer,
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: colorShceme.onSecondaryContainer,
+              fontSize: 15,
+            ),
+          ),
     ),
     home: const Expenses(),
   ));
