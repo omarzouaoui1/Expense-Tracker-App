0473efe3664573a36b8147104c791e89b1fb2aca
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-14 09:34:20 +0100

    done

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 var colorShceme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 61, 116, 167),
@@ -11,53 +12,63 @@ var darkScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(MaterialApp(
-    darkTheme: ThemeData.dark().copyWith(
-      useMaterial3: true,
-      colorScheme: darkScheme,
-      cardTheme: const CardTheme().copyWith(
-        color: darkScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 9,
-        ),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            backgroundColor: darkScheme.primaryContainer,
-            foregroundColor: darkScheme.onPrimaryContainer),
-      ),
-    ),
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData().copyWith(
-      useMaterial3: true,
-      colorScheme: colorShceme,
-      appBarTheme: const AppBarTheme().copyWith(
-        backgroundColor: colorShceme.onPrimaryContainer,
-        foregroundColor: colorShceme.primaryContainer,
-        // centerTitle: true,
-      ),
-      cardTheme: const CardTheme().copyWith(
-        color: colorShceme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 9,
-        ),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: colorShceme.primaryContainer,
-        ),
-      ),
-      textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: colorShceme.onSecondaryContainer,
-              fontSize: 15,
-            ),
+  //Locking the Device Orientation
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then(
+  //   (fn) {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: darkScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: darkScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 9,
           ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: darkScheme.primaryContainer,
+              foregroundColor: darkScheme.onPrimaryContainer),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: colorShceme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: colorShceme.onPrimaryContainer,
+          foregroundColor: colorShceme.primaryContainer,
+          // centerTitle: true,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: colorShceme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 9,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: colorShceme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: colorShceme.onSecondaryContainer,
+                fontSize: 15,
+              ),
+            ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
     ),
-    themeMode: ThemeMode.system,
-    home: const Expenses(),
-  ));
+  );
 }
+//   );
+// }
--- expense_tracker_app/lib/widgets/expenses.dart
+++ expense_tracker_app/lib/widgets/expenses.dart
@@ -41,6 +41,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
@@ -82,6 +83,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text(
         'No expenses found. Start adding some!',
@@ -96,21 +99,27 @@ class _ExpensesState extends State<Expenses> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Expense Tracker'),
-        actions: [
-          IconButton(
-            onPressed: _openAddExpenseOverlay,
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter Expense Tracker'),
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(Icons.add),
+            )
+          ],
+        ),
+        body: width < 600
+            ? Column(
+                children: [
+                  Chart(expenses: _registeredExpenses),
+                  Expanded(child: mainContent),
+                ],
+              )
+            : Row(
+                children: [
+                  Chart(expenses: _registeredExpenses),
+                  Expanded(child: mainContent),
+                ],
+              ));
   }
 }
